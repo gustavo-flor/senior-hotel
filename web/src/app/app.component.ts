20973f81f187194fee7a0cf619db24ef53ef38ad
ogustaflor <ogustaflor@gmail.com>
2020-11-28 00:59:57 +0000

    :tada: Hello Angular App

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './styles/banner-container.sass',
+    './styles/landing-container.sass'
+  ]
+})
+
+export class AppComponent { }
